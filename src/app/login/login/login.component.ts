3f6c6f1f6dfbed6cd52d43bfc3f8a2716a61f74f
Alexandre Behaghel <alexandre.behaghel@club-internet.fr>
2017-12-05 22:57:06 +0100

    sweet login page

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -9,6 +9,40 @@ export class LoginComponent implements OnInit {
 
   constructor() { }
 
+  link = {
+    signText: 'Pas encore inscrit ?',
+    linkText: 'Déjà inscrit ?',
+    title: 'Connexion',
+    signin: true
+  }
+  changeLink(event) {
+    event.preventDefault()
+    this.link.signin = !this.link.signin
+    if (this.link.signin) {
+      this.link.signText = 'Pas encore inscrit ?'
+      this.link.linkText = "S'inscrire"
+      this.link.title = 'Connexion'
+    } else {
+      this.link.signText = 'Déjà inscrit ?'
+      this.link.linkText = 'Se connecter'
+      this.link.title = 'Inscription'
+    }
+  }
+
+  submit() {
+    if (this.link.signin) {
+      this.signin()
+    } else {
+      this.signup()
+    }
+  }
+  signin() {
+    console.log('signin')
+  }
+  signup() {
+    console.log('signup')
+  }
+
   ngOnInit() {
   }
 
